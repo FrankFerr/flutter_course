5e934a96d798adb7717be0d5114a43e96f28d2ea
Francesco Ferrante pc <francescoferrante787@gmail.com>
2026-05-24 23:18:51 +0200

    saved selected date

--- _05_expense_tracker/lib/widgets/expense/new_expense.dart
+++ _05_expense_tracker/lib/widgets/expense/new_expense.dart
@@ -1,6 +1,9 @@
 import 'package:_05_expense_tracker/widgets/utils/number_editing_controller.dart';
 import 'package:_05_expense_tracker/widgets/utils/number_field.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd('it_IT');
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -14,6 +17,7 @@ class NewExpense extends StatefulWidget {
 class _NewExpense extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = NumberEditingController();
+  DateTime? _selectedDate;
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +44,11 @@ class _NewExpense extends State<NewExpense> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text('Selected date'),
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected'
+                          : formatter.format(_selectedDate!),
+                    ),
                     IconButton(
                       onPressed: _onPressedCalendarIcon,
                       icon: const Icon(Icons.calendar_month),
@@ -80,14 +88,18 @@ class _NewExpense extends State<NewExpense> {
     super.dispose();
   }
 
-  void _onPressedCalendarIcon() {
+  void _onPressedCalendarIcon() async {
     final DateTime now = DateTime.now();
 
-    showDatePicker(
+    DateTime? pickedDate = await showDatePicker(
       context: context,
       initialDate: now,
       firstDate: DateTime(now.year - 1),
       lastDate: now,
     );
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
 }
