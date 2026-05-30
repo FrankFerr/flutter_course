83b3652ff2b7fa4cd2df02db4d2d25e5ae03a6e7
Francesco Ferrante <francescoferrante787@gmail.com>
2026-05-30 18:02:05 +0200

    define kColorScheme for improved theming

--- _05_expense_tracker/lib/main.dart
+++ _05_expense_tracker/lib/main.dart
@@ -2,9 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:_05_expense_tracker/widgets/expense/expenses.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+final kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 51, 182, 106),
+);
+
 void main() {
-  initializeDateFormatting(
-    'it_IT',
-    null,
-  ).then((_) => runApp(const MaterialApp(home: Expenses())));
+  initializeDateFormatting('it_IT', null).then(
+    (_) => runApp(
+      MaterialApp(
+        home: const Expenses(),
+        theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+      ),
+    ),
+  );
 }
