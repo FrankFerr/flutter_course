a24ff738f481ced40f6b59e7782ca8967128c9db
Francesco Ferrante pc <francescoferrante787@gmail.com>
2026-06-07 12:48:04 +0200

    refactor main function to use async/await for date formatting initialization

--- _06_expense_tracker_responsive/lib/main.dart
+++ _06_expense_tracker_responsive/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:_06_expense_tracker_responsive/widgets/expense/expenses.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:_06_expense_tracker_responsive/theme/app_theme.dart';
+import 'package:flutter/services.dart';
 
 final kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 51, 182, 106),
@@ -17,14 +18,23 @@ AppTheme appTheme = AppTheme(
   darkColorScheme: kDarkColorScheme,
 );
 
-void main() {
-  initializeDateFormatting('it_IT', null).then(
-    (_) => runApp(
-      MaterialApp(
-        home: const Expenses(),
-        theme: appTheme.lightTheme,
-        darkTheme: appTheme.darkTheme,
-      ),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Future<void> preferredOrientation = SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]);
+  // Future<void> initDateFormatting = initializeDateFormatting('it_IT', null);
+
+  // await Future.wait([preferredOrientation, initDateFormatting]);
+
+  await initializeDateFormatting('it_IT', null);
+
+  runApp(
+    MaterialApp(
+      home: const Expenses(),
+      theme: appTheme.lightTheme,
+      darkTheme: appTheme.darkTheme,
     ),
   );
 }
